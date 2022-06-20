2291ff999418cd50324ca8d67ce654d49c912565
Grasiela Gomes <produtoradama@gmail.com>
2022-06-20 17:17:05 -0300

    test: add util functions unit tests

--- tests/utils/Utils.test.ts
+++ tests/utils/Utils.test.ts
@@ -1,4 +1,9 @@
-import { calcStartTransaction, calcEndTransaction } from "../../src/utils";
+import {
+  calcStartTransaction,
+  calcEndTransaction,
+  formatCurrency,
+  filterTransactions
+} from "../../src/utils";
 
 describe("test utils", () => {
   const page = 6;
@@ -9,5 +14,43 @@ describe("test utils", () => {
 
   it("should return end pagination value", () => {
     expect(calcEndTransaction(page)).toEqual(30);
+  });
+
+  it("should return formatted currency", () => {
+    expect(formatCurrency("12300", "en-US", "USD")).toEqual("$123.00");
+  });
+
+  it("should filter transactions by date and type", () => {
+    const transactions = [
+      {
+        amount: "100",
+        currencyRate: 48000,
+        date: "2020-01-01 08:02:17",
+        from: "Bitcoin",
+        id: "1",
+        to: "USD",
+        totalAmount: "200",
+        status: "LIVE"
+      },
+      {
+        amount: "100",
+        currencyRate: 3.45,
+        date: "2020-01-01 08:02:17",
+        from: "Ethereum",
+        id: "2",
+        to: "EUR",
+        totalAmount: "145",
+        status: "EXCHANGED"
+      }
+    ];
+
+    const filteredList = filterTransactions({
+      endDate: "2020-01-01 08:02:17",
+      startDate: "2020-01-01 08:02:17",
+      transactions,
+      type: "LIVE"
+    });
+
+    expect(filteredList.length).toEqual(1);
   });
 });
